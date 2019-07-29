d79c99a4da5fb6f433e37f1f5b553386bfa35ecc
SethmaWattegedara <sethlewa@gmail.com>
2019-07-29 16:15:41 +0530

    test

---
+++ app/src/main/java/com/example/student/myapplication1/MainActivity.java
@@ -0,0 +1,8 @@
+package com.example.student.myapplication1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String Number 1 = "1";
